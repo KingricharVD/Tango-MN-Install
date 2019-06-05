9dfb341cc1a9a4aef708a52d64f9daf45d47189f
KingricharDV#0439 <northerncommunity1@gmail.com>
2019-06-05 15:54:57 -0400

    Update multi.sh

--- multi.sh
+++ multi.sh
@@ -170,27 +170,27 @@ echo "addnode=80.211.201.13" >> /home/tango4/.tango/tango.conf
 sleep 5 
 fi
 echo "Syncing first node, please wait...";
-northernd -datadir=/home/tango/.tango -daemon
+tangod -datadir=/home/tango/.tango -daemon
 sleep 10 
-until northern-cli -datadir=/home/tango/.tango mnsync status | grep -m 1 '"IsBlockchainSynced": true,'; do sleep 1 ; done > /dev/null 2>&1
+until tango-cli -datadir=/home/tango/.tango mnsync status | grep -m 1 '"IsBlockchainSynced": true,'; do sleep 1 ; done > /dev/null 2>&1
 echo -e ${GREEN}"First node is fully synced. You 1st masternode is running!"${NC}
 sleep 10
 echo "Syncing second node, please wait...";
-northernd -datadir=/home/tango2/.tango -daemon
+tangod -datadir=/home/tango2/.tango -daemon
 sleep 10 
-until northern-cli -datadir=/home/tango2/.tango mnsync status | grep -m 1 '"IsBlockchainSynced": true,'; do sleep 1 ; done > /dev/null 2>&1
+until tango-cli -datadir=/home/tango2/.tango mnsync status | grep -m 1 '"IsBlockchainSynced": true,'; do sleep 1 ; done > /dev/null 2>&1
 echo -e ${GREEN}"Second node is fully synced. You second masternode is running!"${NC}
 sleep 10
 echo "Syncing third node, please wait...";
-northernd -datadir=/home/tago3/.tango -daemon
+tangod -datadir=/home/tago3/.tango -daemon
 sleep 10 
-until northern-cli -datadir=/home/tango3/.tango mnsync status | grep -m 1 '"IsBlockchainSynced": true,'; do sleep 1 ; done > /dev/null 2>&1
+until tango-cli -datadir=/home/tango3/.tango mnsync status | grep -m 1 '"IsBlockchainSynced": true,'; do sleep 1 ; done > /dev/null 2>&1
 echo -e ${GREEN}"Third node is fully synced. You third masternode is running!"${NC}
 sleep 10
 echo "Syncing fourth node, please wait...";
-northernd -datadir=/home/tango4/.tango -daemon
+tangod -datadir=/home/tango4/.tango -daemon
 sleep 10 
-until northern-cli -datadir=/home/tango4/.tango mnsync status | grep -m 1 '"IsBlockchainSynced": true,'; do sleep 1 ; done > /dev/null 2>&1
+until tango-cli -datadir=/home/tango4/.tango mnsync status | grep -m 1 '"IsBlockchainSynced": true,'; do sleep 1 ; done > /dev/null 2>&1
 echo -e ${GREEN}"Last node is fully synced. You fourth masternode is running!"${NC}
 echo ""
 echo -e ${GREEN}"Congrats! Your TNGO Masternodes are now installed and started. Please wait from 10-20 minutes in order to give the masternode enough time to sync, then start the node from your wallet, Debug console option"${NC}
